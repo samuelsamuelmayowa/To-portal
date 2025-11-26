083a3a99103f52dbc87de8678e11e0255196f00e
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-26 15:10:30 +0100

    babs

--- src/components/QuizResults.jsx
+++ src/components/QuizResults.jsx
@@ -4,7 +4,7 @@ const QuizResults = () => {
   const api = import.meta.env.VITE_HOME_OO;
   const storedUser = localStorage.getItem("user");
   let userEmail = "";
-
+ const [darkMode, setDarkMode] = useState(false);
   try {
     const parsed = JSON.parse(storedUser);
     userEmail = parsed?.email || parsed?.username || "";
@@ -58,6 +58,51 @@ const QuizResults = () => {
     );
 
   return (
+    <>
+         <div className="max-w-7xl mx-auto mb-6">
+            <div className="bg-white dark:bg-gray-900 rounded-2xl p-6 shadow flex items-center justify-between">
+              {/* LEFT SIDE - TITLE */}
+              <div>
+                <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+                  To-Analytics Learning Portal
+                </h1>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  Professional Splunk Bootcamp Dashboard
+                </p>
+              </div>
+    
+              {/* RIGHT SIDE - ACTIONS */}
+              <div className="flex items-center gap-4">
+                {/* User Email */}
+                <div className="text-sm text-gray-600 dark:text-gray-300">
+                  {userEmail}
+                </div>
+    
+                {/* Profile Button */}
+                {/* <button
+                  onClick={() => setProfileOpen(true)}
+                  className="px-3 py-1 text-sm rounded bg-blue-600 text-white hover:bg-blue-700 transition"
+                >
+                  My Profile
+                </button> */}
+    
+    <DashboardDropdown/>
+                {/* Dark Mode Toggle */}
+                <button
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="px-3 py-1 rounded text-sm 
+            bg-gray-200 text-gray-800 
+            dark:bg-gray-800 dark:text-white
+            hover:opacity-90 transition"
+                >
+                  {darkMode ? "Light Mode" : "Dark Mode"}
+                </button>
+              </div>
+            </div>
+          </div>
+    
+    
+   
     <div className="p-6 bg-gray-900 text-white rounded-lg max-w-3xl mx-auto mt-20 shadow-xl">
       <h2 className="text-2xl font-bold mb-6 text-center text-indigo-400">
         Your Past Quiz Scores 🧠
@@ -99,6 +144,7 @@ const QuizResults = () => {
         </tbody>
       </table>
     </div>
+     </>
   );
 };
 
