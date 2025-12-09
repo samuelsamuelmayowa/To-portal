203df59ed4efec93074ad0d8723fdf0e9759a76a
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-12-09 12:54:07 +0100

    stock moved

--- src/components/DropdownStock.jsx
+++ src/components/DropdownStock.jsx
@@ -39,6 +39,13 @@ const Dropdownstock = () => {
           >
             Stock prices
           </NavLink>
+ <NavLink
+            to="/dashboard/stock"
+            className="block px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-800 dark:text-gray-200 transition-all duration-200"
+            onClick={() => setOpen(false)}
+          >
+            Stock's 
+          </NavLink>
 
           {/* <NavLink
             to="/dashboard/classmaterials"
