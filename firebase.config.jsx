f20e93dcd54c05e269c3c173b59b1c845187a638
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-12-30 09:48:18 +0100

    https://to-backendapi-v1.onrender.com

--- firebase.config.jsx
+++ firebase.config.jsx
@@ -10,13 +10,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVYLFmSx36EPR5Hi4UAAUiPo_oGoSPgUw",
-  authDomain: "to-api-6a8b5.firebaseapp.com",
-  projectId: "to-api-6a8b5",
-  storageBucket: "to-api-6a8b5.appspot.com",
-  messagingSenderId: "68568748353",
-  appId: "1:68568748353:web:8ce4398e55bbf21aae91c0",
-  measurementId: "G-WW8CJFVZCL"
+  // apiKey: "AIzaSyBVYLFmSx36EPR5Hi4UAAUiPo_oGoSPgUw",
+  // authDomain: "to-api-6a8b5.firebaseapp.com",
+  // projectId: "to-api-6a8b5",
+  // storageBucket: "to-api-6a8b5.appspot.com",
+  // messagingSenderId: "68568748353",
+  // appId: "1:68568748353:web:8ce4398e55bbf21aae91c0",
+  // measurementId: "G-WW8CJFVZCL"
+  apiKey: "AIzaSyDQcOL2CB4f8cYB5zyenvr-LiYn9FGfYxw",
+  authDomain: "to-backend-50da6.firebaseapp.com",
+  projectId: "to-backend-50da6",
+  storageBucket: "to-backend-50da6.firebasestorage.app",
+  messagingSenderId: "74029561052",
+  appId: "1:74029561052:web:813cc92877d47b98163019",
+  measurementId: "G-94K6QBCFXF"
 };
 
 // Initialize Firebase
