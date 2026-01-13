4508e6c773d6e280bddaa7dfb1313a3fcce36828
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-01-13 19:02:38 +0100

    s

--- src/lib/massive.jsx
+++ src/lib/massive.jsx
@@ -4,7 +4,7 @@ const BASE = import.meta.env.VITE_MASSIVE_BASE;
 const API_KEY = import.meta.env.VITE_MASSIVE_API_KEY
 
 export const massive = axios.create({
-  baseURL: "https://api.massive.com/v3",
+  baseURL: "https://api.massive.com/v3/reference",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
