94c2185894bfbc03d71a1800f1f2fe1412dc4f29
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-09-09 12:10:13 +0100

    new base

--- src/components/StockPortal.jsx
+++ src/components/StockPortal.jsx
@@ -39,7 +39,7 @@ const stockCourses = [
           {
             id: "stock-v1",
             title: "Understanding How The Stock Market Works",
-            
+            url: "https://player.vimeo.com/video/1225104800",
             // url: "https://player.vimeo.com/video/1157909911",
           },
         ],
